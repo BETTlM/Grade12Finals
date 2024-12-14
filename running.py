83ce6f3e961bc8cbde127b02664826903fbffba0
Bettim <124069030+BETTlM@users.noreply.github.com>
2024-12-14 18:35:09 +0530

    MacOs Config

--- running.py
+++ running.py
@@ -6,8 +6,8 @@ pgm_2 = r'trial.py'
 
 pgm_3 = r'gui.py'
 
-subprocess.Popen(['python',pgm_1])
+subprocess.Popen(['python3',pgm_1])
 
-subprocess.Popen(['python',pgm_2])
+subprocess.Popen(['python3',pgm_2])
 
-subprocess.Popen(['python', pgm_3])
+subprocess.Popen(['python3', pgm_3])
